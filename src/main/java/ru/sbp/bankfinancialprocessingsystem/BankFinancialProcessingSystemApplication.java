432ef52d1dab573b14b638012ee6652fa7ac8e82
Sergey Vasiliev <vasiljevserj777@gmail.com>
2022-03-05 19:15:17 +0300

    test

--- src/main/java/ru/sbp/bankfinancialprocessingsystem/BankFinancialProcessingSystemApplication.java
+++ src/main/java/ru/sbp/bankfinancialprocessingsystem/BankFinancialProcessingSystemApplication.java
@@ -9,6 +9,8 @@ public class BankFinancialProcessingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankFinancialProcessingSystemApplication.class, args);
+		System.out.println("Ghbdetn MIR");
+		System.out.println("Ghbdetn MIR");
 	}
 
 }
